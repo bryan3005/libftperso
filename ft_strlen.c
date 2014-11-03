53f0211a08490112a1938a5cf93d509fd6625dfa
Bryan Maruani <bryan.maruani9@gmail.com>
2014-11-03 14:33:46 +0100

    modif prototypage

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,11 +6,13 @@
 /*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 11:31:32 by mbryan            #+#    #+#             */
-/*   Updated: 2014/11/03 11:48:16 by mbryan           ###   ########.fr       */
+/*   Updated: 2014/11/03 14:33:08 by mbryan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_strlen(char *str)
+#include <string.h>
+
+size_t ft_strlen(const char *str)
 {
 	int i;
 
