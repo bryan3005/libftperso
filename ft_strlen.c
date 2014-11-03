20d48650ccf61a92aae98a45b835c0ce37f401aa
Bryan Maruani <bryan.maruani9@gmail.com>
2014-11-03 11:49:41 +0100

    modif de ft_strlen (norme check)

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,11 +6,9 @@
 /*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 11:31:32 by mbryan            #+#    #+#             */
-/*   Updated: 2014/11/03 11:39:10 by mbryan           ###   ########.fr       */
+/*   Updated: 2014/11/03 11:48:16 by mbryan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 int ft_strlen(char *str)
 {
@@ -19,12 +17,5 @@ int ft_strlen(char *str)
 	i = 0;
 	while (str[i] != '\0')
 		i++;
-	return i;
-}
-
-
-int main(void)
-{
-	printf("%d\n",ft_strlen("bryan"));
-	return 0;
+	return (i);
 }
