337c0899c9355da6a030d9b7c3042c56cb52cb2f
Bryan Maruani <bryan.maruani9@gmail.com>
2014-11-04 14:45:10 +0100

    ajout de ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/04 14:33:44 by mbryan            #+#    #+#             */
+/*   Updated: 2014/11/04 14:44:10 by mbryan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	char	*ptr;
+
+	i = 0;
+	ptr = (char *)malloc (ft_strlen(s1) * sizeof(char));
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	return (ptr);
+}
