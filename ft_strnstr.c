836d5c6334374ba7988c1014787e67ecfbd36659
Bryan Maruani <bryan.maruani9@gmail.com>
2014-11-04 14:31:57 +0100

    ajout de ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/04 14:17:37 by mbryan            #+#    #+#             */
+/*   Updated: 2014/11/04 14:31:05 by mbryan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <string.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strnstr(const char *str1, const char *str2, size_t n)
+{
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+
+	i = 0;
+	j = 0;
+	ptr = (char *)malloc (ft_strlen(str1) * sizeof(char));
+	while (str1[i])
+	{
+		ptr[i] = str1[i];
+		i++;
+	}
+	i = 0;
+	while (str1[i] && i < n)
+	{
+		while (ptr[i + j] == str2[j] && i + j < n)
+			j++;
+		if (str2[j] == '\0' && ptr)
+			return (ptr + i);
+		else if (str2[j - 1] == '\0' && ptr != '\0')
+			return (ptr + i);
+		j = 0;
+		i++;
+	}
+	return (NULL);
+}
