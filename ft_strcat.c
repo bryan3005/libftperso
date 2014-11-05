133a564b04f80949ee34eddd8e19842b27dd82c2
Bryan Maruani <bryan.maruani9@gmail.com>
2014-11-05 09:55:55 +0100

    ajout de ft_strcat

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,21 +6,55 @@
 /*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 00:30:18 by mbryan            #+#    #+#             */
-/*   Updated: 2014/11/05 00:46:18 by mbryan           ###   ########.fr       */
+/*   Updated: 2014/11/05 09:55:17 by mbryan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-char *ft_strcat(char *s1, char *s2)
+size_t	ft_strlen(const char *str)
 {
-	int i;
-	int j;
+	size_t i;
 
-	j = 0;
 	i = 0;
-	while (s1[i])
+	while (str[i] != '\0')
 		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	char	*ptr;
+
+	i = 0;
+	ptr = (char *)malloc (ft_strlen(s1) * sizeof(char));
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strcat(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*ptr;
+
+	i = 0;
+	j = 0;
+	ptr = ft_strdup(s1);
+	s1 = (char *)malloc ((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char));
+	while (ptr[i])
+	{
+		s1[i] = ptr[i];
+		i++;
+	}
 	while (s2[j])
 	{
 		s1[i] = s2[j];
@@ -29,10 +63,4 @@ char *ft_strcat(char *s1, char *s2)
 	}
 	s1[i] = '\0';
 	return (s1);
-}
-
-int main(void)
-{
-	printf("%s\n",ft_strcat("bonjour  sr rsv", "les amies") );
-	return (0);
 }
