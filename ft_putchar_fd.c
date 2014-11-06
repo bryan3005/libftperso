bea815d4913d898861e16b7368c33ebc6db70b10
Bryan Maruani <bryan.maruani9@gmail.com>
2014-11-06 17:24:43 +0100

    ajout de ft_putchar_fd.c

---
+++ ft_putchar_fd.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/06 17:10:49 by mbryan            #+#    #+#             */
+/*   Updated: 2014/11/06 17:24:02 by mbryan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
