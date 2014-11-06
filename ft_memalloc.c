3fe015b593c9c2e22b7970b4944a36d76266ad70
Bryan Maruani <bryan.maruani9@gmail.com>
2014-11-06 09:02:51 +0100

    ajout de ft_memalloc.c norme ok

---
+++ ft_memalloc.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/06 08:39:28 by mbryan            #+#    #+#             */
+/*   Updated: 2014/11/06 09:02:01 by mbryan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	e;
+	unsigned char	*cb;
+	size_t			i;
+
+	cb = (unsigned char *)b;
+	e = (unsigned char)c;
+	i = 0;
+	while (len > 0)
+	{
+		cb[i] = e;
+		i++;
+		len--;
+	}
+	return (b);
+}
+
+void	*ft_memalloc(size_t size)
+{
+	void *memaloc;
+
+	memaloc = (void *)malloc (size);
+	if (memaloc == NULL)
+		return (NULL);
+	ft_memset(memaloc, 0, size);
+	return (memaloc);
+}
