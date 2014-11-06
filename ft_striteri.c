0dcf52fda3977685f91d0925517c40fdeb776873
Bryan Maruani <bryan.maruani9@gmail.com>
2014-11-06 18:15:49 +0100

    modif 06/11

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 11:12:12 by mbryan            #+#    #+#             */
-/*   Updated: 2014/11/06 11:39:48 by mbryan           ###   ########.fr       */
+/*   Updated: 2014/11/06 18:06:43 by mbryan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <string.h>
 #include <stdio.h>
 
-void	ft_striteri(char *s, void (*f)(unsigned int , char *))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int index;
 
