158d5c6b1e79779a55bcf76bd49968a1bd393372
Bryan Maruani <bryan.maruani9@gmail.com>
2014-11-07 16:36:53 +0100

    modif prof de ft_bezero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,16 +6,15 @@
 /*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 11:57:11 by mbryan            #+#    #+#             */
-/*   Updated: 2014/11/07 16:33:54 by mbryan           ###   ########.fr       */
+/*   Updated: 2014/11/07 16:36:25 by mbryan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	ft_bzero(void *s, size_t nn)
+void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
-	char	*str;
+	size_t i;
 
 	i = 0;
 	str = s;
