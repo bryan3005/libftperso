53967a4e0f0d97686f652ed7a840a78c5a497eeb
bryan3005 <bryanmaruani@pc2.home>
2014-11-09 00:08:39 +0100

    prise en compte des nombres neg

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mbryan <mbryan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 14:29:10 by mbryan            #+#    #+#             */
-/*   Updated: 2014/11/03 15:01:53 by mbryan           ###   ########.fr       */
+/*   Updated: 2014/11/09 00:07:31 by mbryan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,14 @@ int ft_atoi(const char *str)
 
 	i = 0;
 	result = 0;
-	while (str[i] >= '0' && str[i] <= '9')
+	if (str[0] == '-')
+		i++;
+	while ((str[i] >= '0' && str[i] <= '9'))
 	{
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
+	if (str[0] == '-')
+		result *= -1;
 	return (result);
 }
